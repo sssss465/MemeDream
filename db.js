336f0ca9ae73f0403292249b7e15fdbd8f6ff8d1
Andrew M Huang <amh877@linax2.cims.nyu.edu>
2017-11-07 04:37:48 -0500

    added server stuff

--- db.js
+++ db.js
@@ -26,4 +26,22 @@ Picture.plugin(URLSlugs('name'));
 mongoose.model('User', User);
 mongoose.model('Picture', Picture);
 
-mongoose.connect('mongodb://localhost/final');
+// is the environment variable, NODE_ENV, set to PRODUCTION? 
+if (process.env.NODE_ENV === 'PRODUCTION') {
+// if we're in PRODUCTION mode, then read the configration from a file
+// use blocking file io to do this...
+const fs = require('fs');
+const path = require('path');
+const fn = path.join(__dirname, 'config.json');
+const data = fs.readFileSync(fn);
+
+// our configuration file will be in json, so parse it and set the
+// conenction string appropriately!
+const conf = JSON.parse(data);
+dbconf = conf.dbconf;
+} else {
+// if we're not in PRODUCTION mode, then use
+dbconf = 'mongodb://localhost/final';
+}
+
+mongoose.connect(dbconf);
