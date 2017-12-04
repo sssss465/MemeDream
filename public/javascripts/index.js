1807bb125f01697b95ec9d9988e8822a6f08a209
Andrew <sssss465@gmail.com>
2017-12-04 10:13:33 -0500

    polyfill and async await works

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -9,18 +9,27 @@ $(document).ready(function(){
      $('.carousel').carousel();
 });
 /////////////////// ajax for upvoting pictures
-$('#upvote').click(function(){
-  axios.post(window.location.pathname + '/upvote', {
-      upvoted: true
-    })
-    .then(function (response) {
-      // {votes: number}
-      console.log(response);
-      Materialize.toast('Upvoted!', 4000)
-      console.log(response.data);
-      $('#votes')[0].textContent = "Votes: " + response.data.votes;
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+$('#upvote').click(async function (){
+  try {
+    const response = await axios.post(window.location.pathname + '/upvote', {upvoted: true});
+    console.log(response);
+    Materialize.toast('Upvoted!', 4000)
+    console.log(response.data);
+    $('#votes')[0].textContent = "Votes: " + response.data.votes;
+  } catch (err){
+    console.log(err);
+  }
+  // axios.post(window.location.pathname + '/upvote', {
+  //     upvoted: true
+  //   })
+  //   .then(function (response) {
+  //     // {votes: number}
+  //     console.log(response);
+  //     Materialize.toast('Upvoted!', 4000)
+  //     console.log(response.data);
+  //     $('#votes')[0].textContent = "Votes: " + response.data.votes;
+  //   })
+  //   .catch(function (error) {
+  //     console.log(error);
+  //   });
 });
--- webpack.config.js
+++ webpack.config.js
@@ -5,32 +5,42 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
-entry: './public/javascripts/index.js',
-output: {
+  // entry: './public/javascripts/index.js',
+  entry: ["babel-polyfill", "./public/javascripts/index.js"],
+  output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'build.js'
-},
-module: {
+  },
+  module: {
     rules: [{
-            test: /\.css$/,
-            use: ExtractTextPlugin.extract({
-                fallback: "style-loader",
-                use: "css-loader"
-            })
-        },
-        {
-            test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-            loader: 'url-loader?limit=10000&mimetype=application/font-woff'
-        },
-        {
-            test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-            loader: 'file-loader'
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
         }
+      }, {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        })
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader'
+      }
     ]
-},
-plugins: [
+  },
+  plugins: [
     new ExtractTextPlugin("style.css"),
     new UglifyJsPlugin(),
     new OptimizeCssAssetsPlugin()
-]
+  ]
 };
